b97f6c10a1d2f253d06fc24a5fe3194116abf94a
Lucendio <dev@lucend.io>
2017-10-15 00:17:47 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# composable-prototypal-inheritance
-ECMA Proposal - Making prototypal Inheritance composable
+Proposal: `Object.compose` (composable prototypal Inheritance)
+==============================================================
+
+
+
+It is known that `Object.assign` is 
+[not suitable](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Description)
+for *Object Composition*. By design, which I had to learn the hard way. I tried to do this:
+
+```javascript
+import EventMachine from './libs/event-machine';
+import ReactiveStateMachine from './libs/reactive-state-machine';
+
+
+function RtcData( id ){
+    const self = this;
+
+    self._id = id;
+    
+    // ...
+
+    EventMachine.call( self );
+    
+    // ...
+}
+
+// (!!!) NOTE: 'assign' does not work, the proposed 'compose' would
+Object.assign( RtcData.prototype, EventMachine.prototype, ReactiveStateMachine.prototype, {
+   
+    constructor: RtcData,
+    
+    get id(){
+        const self = this;
+        const prefix = 'fasel';
+        return `${prefix}-${self._id}`;
+    },
+    
+    send( /* args */ ){
+        // this is a regular enumerable function
+    },    
+    ...
+});
+
+
+
+const rtData = new RtData('foobar');
+console.log(rtData.id); // prints 'undefined' or throws, instead of 'foobar'
+
+rtData.on('someEvent', function(e) {
+    const rtDataContext = this;
+    console.log(rtDataContext.id); // prints 'undefined' or throws, instead of 'foobar' 
+});
+```
+
+Obviously it won't work. While searching for the root cause of this, I also found the proposal of 
+[`Object.getOwnPropertyDescriptors`](https://github.com/tc39/proposal-object-getownpropertydescriptors)
+for a future version of ECMAScript. At the bottom of the `README.md` the author outlines an 
+[example](https://github.com/tc39/proposal-object-getownpropertydescriptors#illustrative-examples)
+which takes `Object.getOwnPropertyDescriptors` a step further and enables exactly the behaviour 
+I was looking for - a *fixed* version of `Object.assign`.
+
+
+
+### Why?
+
++   all the behaviour of inheritance that got syntax-sugared by the introduction of `class` in *ES6* 
+    can be accomplished in a composable and more transparent way with `Object.compose`
++   provides a fully working way of merging not only object instances but complete prototypes, 
+    which, at the end of the day, is still what's powering `class`es under the hood.
+    
+
+ 
+### Implementation:
+
+A working polyfill can be found [here](./polyfill_obejct.compose.js). Please note that I did *not* 
+wrote this code, I just assembled it. The actual author is the one who proposed
+[`Object.getOwnPropertyDescriptors`](https://github.com/tc39/proposal-object-getownpropertydescriptors).
---
+++ polyfill_obejct.compose.js
@@ -0,0 +1,105 @@
+
+// src: https://github.com/tc39/proposal-object-getownpropertydescriptors
+// NOTE: uses Object.getOwnPropertyDescriptors reference implementation to polyfill
+
+
+
+(function( globalNamespace ){
+    'use strict';
+
+    if( typeof Array.from !== 'function' ){
+        throw new Error('Incompatible ES version - Array.from required');
+    }
+
+    const Object = globalNamespace.Object;
+
+    // Step 1: polyfilling dependencies
+    if (typeof globalNamespace.Reflect === 'undefined') {
+        globalNamespace.Reflect = {
+            defineProperty: Object.defineProperty,
+            getOwnPropertyDescriptor: Object.getOwnPropertyDescriptor,
+            ownKeys: function ownKeys(genericObject) {
+                const gOPS = Object.getOwnPropertySymbols
+                             || function getOwnPropertySymbols(){ return []; };
+                return Object.getOwnPropertyNames(genericObject)
+                    .concat(gOPS(genericObject));
+            }
+        };
+    }
+
+    if( !Object.hasOwnProperty( 'getOwnPropertyDescriptors' ) ){
+        Object.defineProperty( Object, 'getOwnPropertyDescriptors', {
+            configurable: true,
+            writable: true,
+            value: function getOwnPropertyDescriptors(genericObject) {
+                // Let `obj` be ? `ToObject(O)`
+                if (Object(genericObject) !== genericObject) {
+                    throw new Error('Argument should be an object');
+                }
+
+                // Let `ownKeys` be the result of calling ? `obj.[[OwnPropertyKeys]]()`
+                let ownKeys;
+                try {
+                    ownKeys = Reflect.ownKeys(genericObject);
+                } catch(e) {
+                    throw new Error('Unable to retrieve own keys');
+                }
+
+                // Let `descriptors` be ? `ObjectCreate(%ObjectPrototype%)`
+                let descriptors;
+                try {
+                    descriptors = Object.create(Object.prototype);
+                } catch(e) {
+                    throw new Error('Unable to create an instance of Object.prototype');
+                }
+
+                for (let key of ownKeys) {
+
+                    // Let `desc` be the result of ? `obj.[[GetOwnProperty]](key)`
+                    // Let `descriptor` be ? `FromPropertyDescriptor(desc)`
+                    let descriptor = Reflect.getOwnPropertyDescriptor(genericObject, key);
+
+                    if (typeof descriptor !== 'undefined') {
+                        // Let `status` be the result of ? `CreateDataProperty(descriptors, key, descriptor)`
+                        try {
+                            Reflect.defineProperty(descriptors, key, {
+                                configurable: true,
+                                enumerable: true,
+                                writable: true,
+                                value: descriptor
+                            });
+                        } catch(e) {
+                            throw new Error('Unable to create a data property');
+                        }
+                    }
+                }
+
+                // Return `descriptors`
+                return descriptors;
+            }
+        });
+    }
+
+    // Step 2: adding Object.merge, to fix Object.assign (which does not copy setters & getters)
+    if( !Object.hasOwnProperty( 'compose' ) ){
+        Object.compose = function compose(target, ...sources) {
+            sources.forEach(( source  )=>{
+                let descriptors = Object.keys(source).reduce(( descriptors, key )=>{
+                    descriptors[ key ] = Object.getOwnPropertyDescriptor(source, key);
+                    return descriptors;
+                }, {});
+                // by default, Object.assign copies enumerable Symbols too
+                // so grab and filter Symbols as well
+                Object.getOwnPropertySymbols(source).forEach(( sym )=>{
+                    let descriptor = Object.getOwnPropertyDescriptor(source, sym);
+                    if (descriptor.enumerable) {
+                        descriptors[ sym ] = descriptor;
+                    }
+                });
+                Object.defineProperties(target, descriptors);
+            });
+            return target;
+        };
+    }
+
+}( ( typeof window !== 'undefined' ) ? window : global ));
